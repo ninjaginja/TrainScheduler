44b5144681e6116452080da41f3b40cf3002508a
ninjaginja^C <mollywojcik@Mollys-MBP-2.domain>
2018-01-07 16:21:58 -0700

    base html and css added

---
+++ assets/javascript/logic.js
@@ -0,0 +1,13 @@
+
+
+
+// // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyD61w75ANj6YL53qV6gpmSWULGzZRIJcw0",
+//     authDomain: "trainscheduler-19de8.firebaseapp.com",
+//     databaseURL: "https://trainscheduler-19de8.firebaseio.com",
+//     projectId: "trainscheduler-19de8",
+//     storageBucket: "",
+//     messagingSenderId: "250813293856"
+//   };
+//   firebase.initializeApp(config);
