0ff1509fc3c8beb9da265dba204ba0dc5c397239
Molly Wojcik <mollywojcik@gmail.com>
2018-01-09 20:14:20 -0700

    current version is writing to database and writing to html. Next up: formulas for calculating next arrival and minutes away

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,20 +1,12 @@
 $(document).ready(function() {
 
-  // Function to animate the train in header
-  var width = "+=" + $(document).width();
-  $("#train-image").animate({
-  left: width
-  }, 9000, function() {
-    // Animation complete.
-  });
-
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyD61w75ANj6YL53qV6gpmSWULGzZRIJcw0",
     authDomain: "trainscheduler-19de8.firebaseapp.com",
     databaseURL: "https://trainscheduler-19de8.firebaseio.com",
     projectId: "trainscheduler-19de8",
-    storageBucket: "",
+    storageBucket: "trainscheduler-19de8.appspot.com",
     messagingSenderId: "250813293856"
   };
   firebase.initializeApp(config);
@@ -22,23 +14,53 @@ $(document).ready(function() {
   // Create a variable to reference the database.
   var database = firebase.database();
 
-  // At the initial load and subsequent value changes, get a snapshot of the stored data.
-  // This function allows you to update your page in real-time when the firebase database changes.
-  database.ref().on("value", function(snapshot) {
+  $("#submit-button").on("click", function(event) {
+         // Prevent the page from refreshing
+         event.preventDefault();
 
-    // Set the variables equal to the stored values.
-    var trainName = snapshot.val().trainName;
-    var trainDestination = snapshot.val().trainDestination;
-    var firstTrainTime = snapshot.val().firstTrainTime;
-    var frequency = snapshot.val().frequency;
+         // Get inputs-
+         var trainName = $("#train-name").val().trim();
+         var trainDestination = $("#train-destination").val().trim();
+         var firstTrainTime = $("#first-train-time").val().trim();
+         var frequency = $("#frequency").val().trim();
 
-    // Change the table rows inside the train schedule to reflect the stored values
+         // Change what is saved in firebase
+         database.ref().push({
+           trainName: trainName,
+           trainDestination: trainDestination,
+           firstTrainTime: firstTrainTime,
+           frequency: frequency,
+           dateAdded: firebase.database.ServerValue.TIMESTAMP
+         });
+         // form.empty();
+       });
 
 
-  // If any errors are experienced, log them to console.
-  }, function(errorObject) {
-    console.log("The read failed: " + errorObject.code);
-  });
+   // Firebase watcher + initial loader
+   database.ref().on("child_added", function(childSnapshot) {
+
+     // Log everything that's coming out of snapshot
+     console.log(childSnapshot.val().trainName);
+     console.log(childSnapshot.val().trainDestination);
+     console.log(childSnapshot.val().firstTrainTime);
+     console.log(childSnapshot.val().frequency);
+
+     // Formula to get next arrival time based on firstTrainTime, frequency and current time
+
+
+     // Formula to get next arrival time based on firstTrainTime, frequency and current time
+     // var minutesAway =
+
+     // Change the table rows inside the train schedule to reflect the stored values
+     $("#train-table").append("<div class='row train-schedule'>").append("<div class='col-md-3 train-name'>" + childSnapshot.val().trainName + "</div><div class='col-md-2 destination'>" + childSnapshot.val().trainDestination +
+     "</div><div class='col-md-2 frequency'>" + childSnapshot.val().frequency + "</div></div></li>");
+
+   // Handle the errors
+   }, function(errorObject) {
+     console.log("Errors handled: " + errorObject.code);
+   });
+
+
 
 
 });
